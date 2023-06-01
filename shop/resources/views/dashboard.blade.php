be1fc897fff0464142db3acb2fa90c3d9896f0cb
Antenev <atenev.21@gmail.com>
2023-06-01 14:51:38 +0300

    Initialize the repo

---
+++ shop/resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+    <title>My online shop</title>
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+    <nav>
+        <label class="logo"><a href="/">Shop</a></label>
+        <ul>
+            <li><a class="active" href="/helmets">Helmets</a></li>
+            <li><a href="#">Jackets</a></li>
+            <li><a href="#">Boots</a></li>
+            <li><a href="#">Gloves</a></li>
+            <li><a href="#">Other</a></li>
+        </ul>
+    </nav>
+</body> 
---
+++ shop/resources/views/helmets.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<head>
+    <title>Helmets</title>
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+    <p>Hello world</p>
+</body> 
